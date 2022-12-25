3e41ad32f8c4065e6f43797b58bd0317f89d0d88
fermer123 <fermer_525@mail.ru>
2022-12-25 16:04:39 +0500

    setinput

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,28 @@ import useDebounce from './components/debounce';
 const url =
   'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address';
 const token = '48f272b426d7267c92ceef852fed9aad1f8d4047';
+
 interface IAdress {
   city_with_type: string;
   street_with_type: string;
-  house_type: number;
+  house_type?: string;
+  house: number;
+}
+interface Idata {
+  map(arg: (e: Idata, idx: number) => JSX.Element): React.ReactNode;
+  data: IAdress;
 }
 const App: FC = () => {
-  const [address, setAddress] = useState<IAdress[]>([]);
+  const [address, setAddress] = useState<Idata>(null);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [error, setError] = useState<Error | string>('');
   const [dropdown, setDropdown] = useState<boolean>(false);
   const input = useInput('');
   const debounce = useDebounce(input.value, 600);
 
+  const AddressHandler = (e: React.MouseEvent<HTMLElement>, data: IAdress) => {
+    e.stopPropagation();
+  };
   const options: RequestInit = {
     method: 'POST',
     mode: 'cors',
@@ -28,9 +37,8 @@ const App: FC = () => {
       Accept: 'application/json',
       Authorization: `Token ${token}`,
     },
-    body: JSON.stringify({query: debounce, count: 2}),
+    body: JSON.stringify({query: debounce, count: 3}),
   };
-
   useEffect(() => {
     if (debounce.length > 3) {
       fetch(url, options)
@@ -42,26 +50,30 @@ const App: FC = () => {
       setDropdown(false);
     }
   }, [debounce]);
-  console.log(address);
   return (
     <div className={style.app_container}>
       <div>
         Адрес
         <span>*</span>
       </div>
-      <input {...input} />
-      {dropdown && (
-        <ul>
-          {address.map((e, idx) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <li key={idx}>
-              <div>{e.city_with_type}</div>
-              <div>{e.street_with_type}</div>
-              <div>{e.house_type}</div>
-            </li>
-          ))}
-        </ul>
-      )}
+      <div className={style.app_container_input}>
+        <input {...input} />
+        {dropdown && (
+          <ul>
+            {address.map(({data}: Idata, idx) => (
+              // eslint-disable-next-line react/no-array-index-key, jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/click-events-have-key-events
+              <li
+                onClick={(e) => AddressHandler(e, data)}
+                // eslint-disable-next-line react/no-array-index-key
+                key={idx}>
+                <p>{data.city_with_type ? data.city_with_type : ''}</p>
+                <p>{data.street_with_type ? data.street_with_type : ''}</p>
+                <p>{data.house ? `${data.house_type} ${data.house}` : ''}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
